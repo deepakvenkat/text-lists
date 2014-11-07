154d107c4ce46857759454a545cc2a01715e70a1
deepakvenkat <dipak2911@gmail.com>
2014-11-07 14:48:31 -0500

    Inital commit

---
+++ webserver/app.js
@@ -0,0 +1,31 @@
+var express = require('express'),
+	domain = require("domain").create(),
+	nconf = require("nconf"),
+	app = express(),
+	connect = require("connect"),
+	bodyParser = require("body-parser"),
+	methodOverride = require("method-override"),
+	util = require("util"), 
+	config = require("./config"),
+	appConfig;
+
+
+nconf.argv().env();
+
+domain.on("error", function (err) {
+	console.log(util.inspect(err));
+});
+
+
+function startApp () {
+	appConfig = config[nconf.get("NODE_ENV")] || config["dev"];
+	app.set("view options", {layout: false});
+	app.use(bodyParser());
+	app.use(methodOverride());
+	app.use(app.router);
+	var server = app.listen(appConfig.port, function() {
+		console.log("Listening on port %d", server.address().port);
+	});
+}
+
+domain.run(startApp);
